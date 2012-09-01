875b873caedd4c2b693638390de949f4f5cdf783
Knut <maximilian.kiesel@rwth-aachen.de>
2012-09-01 10:54:14 +0200

    beginning of z-kompensation in protokol

--- teilchen/analyse/calculate.py
+++ teilchen/analyse/calculate.py
@@ -27,6 +27,7 @@ def shortBeta(a,q):
 def arrayToUncertainty( values ):
 	'''
 	values: list of values
+	returns ufloat with mean and std of input data
 	'''
 	from numpy import array
 	data = array( values )
@@ -38,20 +39,26 @@ def arrayToUncertainty( values ):
 
 from math import pi,sqrt
 from numpy import array
-u_g = arrayToUncertainty( [388, 417, 378, 294, 361, 369, 332, 395, 417, 313] )
-print 'u_g: ', u_g
-u_g = ufloat ( ( 130, 20 ))
+from sys import exit
+u_g = []
+u_g.append( arrayToUncertainty( [52, 44, 41, 36, 47, 50] ) )
+u_g.append( arrayToUncertainty( [86, 91, 101, 85, 82, 92, 88, 107] ) )
+u_g.append( arrayToUncertainty( [174, 167, 163, 170, 169, 172, 172, 167, 160] ) )
+u_g.append( arrayToUncertainty( [230, 227, 228, 235, 230, 223, 225, 229] ) )
+u_g.append( arrayToUncertainty( [284, 264, 271, 277, 271, 277, 270, 271, 276] ) )
+
+for u in u_g:
+	printError(u, unit = 'V')
+
+
 d = ufloat( ( 0.00305, 0.00025 ) )# m
 g = 9.81 # m/s²
-e = 1.6e-19
 
-'''# a typical mass
-#r = ufloat((30e-6,25e-6))
-#m = 4./3*pi*r**3*2700 # kg'''
+for u in [ u_g[1], u_g[4] ]:
+	qm_grav = g * d / u
+	printError(qm_grav, unit = 'C/kg')
 
-qm_grav = g * d /u_g
-printError(qm_grav, unit = 'C/kg')
-
+exit()
 #### resonanz ####
 ## ohne luft ##
 w_res = ufloat((5,1))
@@ -71,8 +78,18 @@ printError( qm_res_vak )
 
 ## ohne Luft ##
 from numpy import array
-uxList = array([1230, 1140, 1000])
-ugList = array([115, 100, 85])
+uxList = array([700, 800, 900, 1000, 1100, 1200])
+ugList = array([36, 68, 127, 162, 250, 375])
+
+uxList_drop = array([1280, 1175, 1070, 905])
+uyList_drop = array([79, 68, 50, 44])
+
+x2 = array( [1230, 1140, 1000] )
+y2 = array( [115, 100, 85] )
+
+x3 = array( [1200, 1100, 1000] )
+y3 = array( [280, 260, 320] )
+
 from uncertainties.unumpy import uarray
 x = uarray(( uxList, [10.]*len(uxList)))
 y = uarray(( ugList, [10.]*len(ugList)))
@@ -121,6 +138,10 @@ reg.draw(";12 K U_{x}^{2} / (#Omega^{2} d^{2});U_{g} [V]")
 
 
 
+'''# a typical mass
+#e = 1.6e-19
+#r = ufloat((30e-6,25e-6))
+#m = 4./3*pi*r**3*2700 # kg'''
 
 
 
