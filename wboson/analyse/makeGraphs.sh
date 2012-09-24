42432d845461172dbb583d8c217a238db26a81b0
Knut <maximilian.kiesel@rwth-aachen.de>
2012-09-24 12:23:16 +0200

    met:el_et graph added

--- wboson/analyse/makeGraphs.sh
+++ wboson/analyse/makeGraphs.sh
@@ -3,6 +3,7 @@
 
 ./2dimensionalComparison.py --save
 ./2dimensionalComparison.py -c 'mwt/el_et>1.7' --save
+./2dimensionalComparison.py -p met:el_et --save
 ./tauBackgroundEstimation.py -p mwt --save
 ./tauBackgroundEstimation.py -p met --cutline 30 --save
 ./tauBackgroundEstimation.py -p el_et -c 'met>30' --cutline 25 --save
