6ed6ea17e0e859c44d8ff6d4361792e0935b2edb
Knut <maximilian.kiesel@rwth-aachen.de>
2012-09-27 15:16:51 +0200

    et mit neuem cut

--- wboson/analyse/analyse.py
+++ wboson/analyse/analyse.py
@@ -195,6 +195,10 @@ if (__name__ == "__main__"):
 	mcTree = readTree( opts.mcfile )
 	dataTree = readTree( opts.datafile )
 
+	# set cut for el_et !!!!!!!!!!!!!!!! overwrites all!!!!!!!!!!!!!
+	if opts.plots == "el_et":
+		opts.cut = opts.cut + ' && mwt/el_et>1.9'
+
 	m, e_m,e_m_sys = getMass( dataTree, mcTree, opts.cut, opts.save, opts.plots, opts.quiet )
 	sin2_wein,err_sin2_wein_stat,err_sin2_wein_sys = getWeinberg( m, e_m ,e_m_sys)
 	gamma,err_gamma_stat,err_gamma_sys = getWidth(m, e_m, e_m_sys )
